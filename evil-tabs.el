76173e22cee25dec46123348fa2b4da10ba6b56e
Sean Wilson <spwilson27@gmail.com>
2017-05-22 23:38:03 -0500

    Change from elscreen to eyebrowse

--- evil-tabs.el
+++ evil-tabs.el
@@ -1,69 +1,87 @@
 ;;; evil-tabs.el --- Integrating Vim-style tabs for Evil mode users.
 ;; Copyright 2013 Kris Jenkins
-;;
-;; Author: Kris Jenkins <krisajenkins@gmail.com>
-;; Maintainer: Kris Jenkins <krisajenkins@gmail.com>
-;; Keywords: evil tab tabs vim
-;; URL: https://github.com/krisajenkins/evil-tabs
+;; Copyright 2017 Sean Wilson
+;; 
+;; Author: Sean Wilson <spwilson2@wisc.edu>
+;; Maintainer: Sean Wilson <spwilson2@wisc.edu>
+;; Keywords: evil tab tabs vim eyebrowse
+;; URL: https://github.com/spwilson2/evil-tabs
 ;; Created: 30th September 2013
-;; Version: 0.1.0
-;; Package-Requires: ((evil "0.0.0") (elscreen "0.0.0"))
+;; Version: 0.2.0-dev
+;; Package-Requires: ((evil "0.0.0") (eyebrowse "0.0.0"))
 
 ;;; Commentary:
 ;;
 ;; Integrating Vim-style tabs for Evil mode users.
-
 ;;; Code:
 
 (require 'evil)
-(require 'elscreen)
+(require 'eyebrowse)
 
 (defvar evil-tabs-mode-map (make-sparse-keymap)
   "Evil-tabs-mode's keymap.")
 
-(evil-define-command evil-tabs-tabedit (file)
-  (interactive "<f>")
-  (elscreen-find-file file))
+;; TODO: need to add support for tabe specifying a file
+;; use eyebrowse-new-workspace:
+;(evil-define-command evil-tabs-tabedit (file)
+;  (interactive "<f>")
+;  (elscreen-find-file file))
 
+(evil-define-command evil-tabs-tabedit ()
+  (eyebrowse-create-window-config))
+
+;; Quit only if this is the last tab open, otherwise close this tab.
+;; Todo: Also need sure that this doesn't break window functionality
 (evil-define-command evil-tab-sensitive-quit (&optional bang)
   :repeat nil
   (interactive "<!>")
-  (if (> (length (elscreen-get-screen-list)) 1)
-    (elscreen-kill)
-    (evil-quit bang)))
+  (let ((window-configs (eyebrowse--get 'window-configs)))
+  (if (> (length window-configs) 1)
+    (eyebrowse-close-window-config)
+    (evil-quit bang))))
 
-(evil-define-command evil-tabs-current-buffer-to-tab ()
-  (let ((nwindows (length (window-list)))
-        (cb (current-buffer)))
-    (when (> nwindows 1)
-      (delete-window)
-      (elscreen-create)
-      (switch-to-buffer cb))))
+(evil-define-command evil-tab-close ()
+  (eyebrowse-close-window-config))
 
 (evil-define-motion evil-tabs-goto-tab (&optional count)
   (if count
-     (elscreen-goto (- count 1))
-     (elscreen-next)))
+     (eyebrowse-next-window-config (- count 1))
+     (eyebrowse-next-window-config 1)))
+
 
 (evil-ex-define-cmd "tabe[dit]" 'evil-tabs-tabedit)
-(evil-ex-define-cmd "tabclone" 'elscreen-clone)
-(evil-ex-define-cmd "tabc[lose]" 'elscreen-kill)
-(evil-ex-define-cmd "tabd[isplay]" 'elscreen-toggle-display-tab)
-(evil-ex-define-cmd "tabg[oto]" 'elscreen-goto)
-(evil-ex-define-cmd "tabo[nly]" 'elscreen-kill-others)
-(evil-ex-define-cmd "tabnew" 'elscreen-create)
-(evil-ex-define-cmd "tabn[ext]" 'elscreen-next)
-(evil-ex-define-cmd "tabp[rev]" 'elscreen-previous)
-(evil-ex-define-cmd "tabr[ename]" 'elscreen-screen-nickname)
-(evil-ex-define-cmd "tabs[elect]" 'elscreen-select-and-goto)
-(evil-ex-define-cmd "tabw[ith]" 'elscreen-find-and-goto-by-buffer)
+(evil-ex-define-cmd "tabclone" 'evil-tabs-tabedit)
+(evil-ex-define-cmd "tabc[lose]" 'evil-tab-close)
 (evil-ex-define-cmd "q[uit]" 'evil-tab-sensitive-quit)
 
+; TODO allow numbers
 (evil-define-key 'normal evil-tabs-mode-map
-  "gt" 'elscreen-next
-  "gT" 'elscreen-previous
-  "gt" 'evil-tabs-goto-tab
-  "T" 'evil-tabs-current-buffer-to-tab)
+  "gt" 'eyebrowse-next-window-config
+  "gT" 'eyebrowse-prev-window-config
+;  "gt" 'evil-tabs-goto-tab
+;  "T" 'evil-tabs-current-buffer-to-tab
+  )
+
+;TODO: Close all tabs but 'current-slot
+;(evil-ex-define-cmd "tabo[nly]" 'TODO)
+
+; TODO Moves the current buffer to its own tab
+;(evil-define-command evil-tabs-current-buffer-to-tab ()
+;  (let ((nwindows (length (window-list)))
+;        (cb (current-buffer)))
+;    (when (> nwindows 1)
+;      (delete-window)
+;      (elscreen-create)
+;      (switch-to-buffer cb))))
+;
+;(evil-ex-define-cmd "tabd[isplay]" 'elscreen-toggle-display-tab)
+;(evil-ex-define-cmd "tabg[oto]" 'elscreen-goto)
+;(evil-ex-define-cmd "tabnew" 'elscreen-create)
+;(evil-ex-define-cmd "tabn[ext]" 'elscreen-next)
+;(evil-ex-define-cmd "tabp[rev]" 'elscreen-previous)
+;(evil-ex-define-cmd "tabr[ename]" 'elscreen-screen-nickname)
+;(evil-ex-define-cmd "tabs[elect]" 'elscreen-select-and-goto)
+;(evil-ex-define-cmd "tabw[ith]" 'elscreen-find-and-goto-by-buffer)
 
 ;;;###autoload
 (define-minor-mode evil-tabs-mode
@@ -74,12 +92,15 @@
     (evil-normal-state)
     (evil-change-state prev-state)
 
-    (elscreen-start)))
+    ; Wrap around like vim does for tabs.
+    (make-local-variable 'eyebrowse-wrap-around)
+    (setq eyebrowse-wrap-around t)
+    ))
 
 ;;;###autoload
 (defun turn-on-evil-tabs-mode ()
   "Enable `evil-tabs-mode' in the current buffer."
-  (evil-tabs-mode 1))
+    (evil-tabs-mode 1))
 
 ;;;###autoload
 (defun turn-off-evil-tabs-mode ()
